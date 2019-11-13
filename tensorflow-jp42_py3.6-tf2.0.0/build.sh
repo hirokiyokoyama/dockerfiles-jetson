f2675a4e9196279409f993ca7d33e6108ac273c7
Hiroki Yokoyama <h-yokoyama@craft-server.co.jp>
2019-11-13 13:52:34 +0900

    add TensorFlow2.0

---
+++ tensorflow-jp42_py3.6-tf2.0.0/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp -r `readlink -f /usr/lib/cuda` ./cuda
+cp /usr/lib/aarch64-linux-gnu/tegra/libcuda.so* ./cuda/lib64/
+cp /usr/lib/aarch64-linux-gnu/libcudnn.so.7* ./cuda/lib64/
+#cp /usr/include/aarch64-linux-gnu/cudnn_v7.h ./cuda/include/cudnn.h
+cp /usr/include/cudnn.h ./cuda/include
+docker build -t tensorflow-jp42:py3.6-tf2.0.0 .
+rm -rf ./cuda
+
